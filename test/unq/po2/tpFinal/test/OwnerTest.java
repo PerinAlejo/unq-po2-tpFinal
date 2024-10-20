da66232d8d98580f59cf6faa83fbaa62a9970900
Franco Paredes <francoismaelparedes@hotmail.com>
2024-10-19 21:27:07 -0300

    se finalizaron tests

--- test/unq/po2/tpFinal/test/OwnerTest.java
+++ test/unq/po2/tpFinal/test/OwnerTest.java
@@ -1,18 +1,26 @@
 package unq.po2.tpFinal.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import unq.po2.tpFinal.Owner;
+import unq.po2.tpFinal.*;
 
 public class OwnerTest {
 	private Owner owner;
+	private Tenant mockTenant;
 
     @BeforeEach
     public void setUp() {
     	owner = new Owner("Mr. Pruebbas", "prueba@hotmail.com", "1133445566");
+    	mockTenant = mock(Tenant.class);
     }
 
     @Test
@@ -25,5 +33,23 @@ public class OwnerTest {
     @Test
     public void testGetAverageRatingWithNoRatings() {
         assertEquals(0, owner.getAverageRating());
+    }
+    
+    @Test
+    public void testRankMethodWithMockTenant() {
+        //defino el comportamiento del mock para cuando se llame a addRating y getAverageRating
+        doNothing().when(mockTenant).addRating(anyInt());
+        when(mockTenant.getAverageRating()).thenReturn(4.0);
+
+        //el propietario usa el metodo rank para calificar el mockeado
+        owner.rank(mockTenant, 5);
+        owner.rank(mockTenant, 3);
+
+        //verifico que el metodo addRating se haya llamado dos veces al mock con los valores correctos
+        verify(mockTenant, times(1)).addRating(5);
+        verify(mockTenant, times(1)).addRating(3);
+
+        //verifico que el promedio de calificaciones del mock sea el esperado
+        assertEquals(4.0, mockTenant.getAverageRating(), 0.01);
     }
 }
